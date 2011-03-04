62b67efc4287783a4dfc9abcb6407b08e8990f22
Stéphane Bisinger <stephane.bisinger@gmail.com>
2011-03-04 14:14:49 +0000

    Added opening hours

--- header.php
+++ header.php
@@ -1,6 +1,8 @@
         <div id="container">
             <div id="header" class="central">
-                <div id="logo"><img src="images/logo.jpg" width="310" height="90" alt="DNU"></div>
+                <div id="logo"><img src="images/logo.jpg" width="310" height="90" alt="DNU"><div id="op_hours"><h4>orario apertura impianto</h4>lun-mer-ven: 10:00 - 13:30 / 15:00 - 22:00<br>
+mar-gio: 14:00 - 22:00<br>
+sab: 10:00 - 13:30</div></div>
             </div>
             <div id="menu" class="central">
                         <select id="stylesel">
