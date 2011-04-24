e61bcbec33d38075407f19d60828284896e56b08
Stéphane Bisinger <stephane.bisinger@gmail.com>
2011-04-24 15:25:05 +0100

    Completed description of activities

---
+++ attivita/fun.php
@@ -0,0 +1,54 @@
+<h1>Rieducazione funzionale</h1>
+La rieducazione funzionale in acqua unisce i vantaggi dell’ambiente liquido alla possibilità di iniziare
+precocemente il trattamento di recupero dall' infortunio. L'acqua consente un’ottimizzazione del
+lavoro che porta a ridurre i tempi di recupero ed a limitare le possibilità di comparsa di patologie
+secondarie conseguenti ai carichi di lavoro eseguiti fuori dall’acqua. Il programma riabilitativo
+verrà stilato sulla base delle esigenze di ogni singola persona e sarà seguito dalla nostra equipe
+comprensiva di un esperto di rieducazione funzionale, e di uno psicologo, che farà da supporto al
+paziente nella gestione delle problematiche legate all’infortunio e al recupero.
+
+<h4>ORARI E GIORNI</h4>
+
+DA CONCORDARE
+
+<h4>COSTI</h4>
+<table>
+    <thead>
+        <tr>
+            <th></th>
+            <th>1 ingresso</th>
+            <th>10 ingressi</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td>1° ciclo</td>
+            <td>20&euro;</th>
+            <td>160&euro;</td>
+        </tr>
+        <tr>
+            <td>2° ciclo</td>
+            <td>20&euro;</th>
+            <td>130&euro;</td>
+        </tr>
+        <tr>
+            <td>3° ciclo</td>
+            <td>20&euro;</th>
+            <td>100&euro;</td>
+        </tr>
+        <tr>
+            <td>4° ciclo</td>
+            <td>20&euro;</th>
+            <td>80&euro;</td>
+        </tr>
+    </tbody>
+</table>
+
+<h5>ISCRIZIONE OBBLIGATORIA</h5>
+<ul>
+    <li>18&euro; per gli adulti</li>
+    <li>13&euro; per i minori di 16 anni</li>
+</ul>
+
+<strong>PER ISCRIVERSI E' NECESSARIO MUNIRSI DI CERTIFICATO MEDICO
+DI SANA E ROBUSTA COSTITUZIONE.</strong>
---
+++ attivita/gym.php
@@ -0,0 +1,68 @@
+<h1>Acquafitness</h1>
+<h4>ACQUAFITNESS, PERCHE'</h4>
+<blockquote>
+Il fitness in acqua per tenersi sempre in forma e divertirsi a ritmo di musica
+</blockquote>
+Acquafitness comprende tutte quelle attività in acqua che hanno come finalità principale quella di
+mantenere la forma fisica, migliorarla e potenziarne le capacità.
+I principi del fitness in acqua sono essenzialmente due:
+<ul>
+    <li>L'acqua fa più resistenza ai movimenti rispetto all'aria;</li>
+    <li>La quasi totale assenza di gravità e la posizione verticale in cui si eseguono gli esercizi
+      favoriscono il drenaggio dei liquidi e migliorano la circolazione sanguigna</li>
+</ul>
+I corsi di acquafitness hanno durata di 8 lezioni
+in cui si alternano lavori aerobici e di potenziamento muscolare.
+
+<h4>ORARI E GIORNI</h4>
+<ul>
+    <li>Lunedì e Mercoledì: 20.30 - 21.20</li>
+    <li>Martedì e Giovedì: 19.20 - 20.10</li>
+</ul>
+
+<h4>COSTI</h4>
+<table>
+    <thead>
+        <tr>
+            <th></th>
+            <th>1 ingresso</th>
+            <th>4 ingressi</th>
+            <th>8 ingressi</th>
+            <th>12 ingressi</th>
+            <th>mensile</th>
+            <th>trimestrale</th>
+            <th>annuale</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td>con scadenza</td>
+            <td>&dash;</th>
+            <td>26&euro;<br>(2 settimane)</td>
+            <td>50&euro;<br>(1 mese)</td>
+            <td>70&euro;<br>(2 mesi)</td>
+            <td>50&euro;<br>(1 mese)</td>
+            <td>140&euro;<br>(3 mesi)</td>
+            <td>380&euro;<br>(12 mesi)</td>
+        </tr>
+        <tr>
+            <td>senza scadenza</td>
+            <td>7&euro;</td>
+            <td>28&euro;</td>
+            <td>55&euro;</td>
+            <td>80&euro;</td>
+            <td>&dash;</td>
+            <td>&dash;</td>
+            <td>&dash;</td>
+        </tr>
+    </tbody>
+</table>
+
+<h5>ISCRIZIONE OBBLIGATORIA</h5>
+<ul>
+    <li>18&euro; per gli adulti</li>
+    <li>13&euro; per i minori di 16 anni</li>
+</ul>
+
+<strong>PER ISCRIVERSI E' NECESSARIO MUNIRSI DI CERTIFICATO MEDICO
+DI SANA E ROBUSTA COSTITUZIONE.</strong>
