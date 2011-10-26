c74545cb3b4561f66d8aa1b1d4fe6a71a5ef8bdc
Stéphane Bisinger <stephane.bisinger@gmail.com>
2011-10-26 17:53:42 +0200

    Added map to contact page

--- contatti.php
+++ contatti.php
@@ -11,11 +11,49 @@ require_once "settings.php";
 
 <?php include "styles.php"; ?>
 <?php include "js.php"; ?>
+<script type="text/javascript"
+    src="http://maps.googleapis.com/maps/api/js?sensor=false">
+</script>
+<script type="text/javascript">
+  function initialize() {
+    var latlng = new google.maps.LatLng(43.733763, 12.625493);
+    var myOptions = {
+      zoom: 15,
+      center: latlng,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+    var map = new google.maps.Map(document.getElementById("map_canvas"),
+        myOptions);
+    var infowindow = new google.maps.InfoWindow({
+            content: "Piscina Comunale F.lli Cervi"
+    });
+    var marker = new google.maps.Marker({
+        position: latlng,
+        map: map,
+        title:"Piscina Comunale F.lli Cervi"
+    });
+
+    google.maps.event.addListener(marker, 'click', function() {
+          infowindow.open(map,marker);
+    });
+  }
+
+</script>
 	</head>
-	<body>
+	<body onload="initialize()">
 <?php include "header.php"; ?>
             <div id="body" class="central">
                 <h1>Contatti</h1>
+                <div id="map_canvas" style="width: 600px; height: 400px; float: left;"></div>
+                <address>ASD Ducato Nuoto Urbino<br>
+                Piazzale F.lii Cervi, 1<br>
+                61029 Urbino (PU)<br>
+                tel. 0722 320 834<br>
+                fax. 0722 222 222<br>
+                e-mail: <a href="mailto:ducatonuoto@gmail.com">ducatonuoto@gmail.com</a><br>
+                Pagina Facebook: <a href="http://www.facebook.com/pages/Ducato-Nuoto-Urbino/146388187392">Ducato Nuoto Urbino</a>
+                </address>
+                <div style="clear: both;">
             </div>
 <?php include "footer.php"; ?>
 	</body>
