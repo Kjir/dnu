ffab4240c7086b4c2fb7d687c5a410ec742aff48
Stéphane Bisinger <stephane.bisinger@gmail.com>
2011-11-03 20:58:01 +0100

    Updated activty list

--- attivita/attivita.php
+++ attivita/attivita.php
@@ -1,7 +1,13 @@
 <h1>Attività</h1>
 La Ducato Nuoto Urbino promuove diverse attività adatte ad ogni età e abilità natatorie.
-In particolare vengono proporsi <strong>corsi di avviamento/perfezionamento</strong> del nuoto per <strong>adulti</strong> e per
-<strong>bambini</strong>, lezioni di <strong>acquafitness</strong> per chi vuole tenersi in forma, corsi di <strong>rieducazione
-funzionale</strong> per il recupero da infortuni oppure semplicemente <strong>nuoto libero</strong> per chi vuole nuotare in
-autonomia. La Ducato Nuoto Urbino ha anche una squadra di <strong>agonismo</strong> e una <strong>squadra master</strong> per chi
-vuole misurarsi in competizioni con atleti di ogni età all'interno del clima amichevole di una squadra di nuoto.
+In particolare vengono proposti:
+<ul>
+    <li>Corsi di avviamento/perfezionamento del nuoto per <strong>adulti</strong></li>
+    <li>Corsi di ambientamento e insegnamento del nuoto per
+<strong>bambini</strong></li>
+    <li>Lezioni di <strong>acquafitness</strong> per chi vuole tenersi in forma
+    <li>Corsi di <strong>rieducazione funzionale</strong> per il recupero da infortuni</li>
+     <li>Accesso al <strong>nuoto libero</strong> per chi vuole nuotare in
+autonomia.</li>
+    <li>La Ducato Nuoto Urbino ha anche una <strong>squadra master</strong> per chi vuole misurarsi in competizioni con atleti di ogni età all'interno del clima amichevole di una squadra di nuoto.</li>
+</ul>
