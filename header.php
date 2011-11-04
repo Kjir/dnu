76872b6800ebe9a2f1dec4d1a67c710122fb763f
Stéphane Bisinger <stephane.bisinger@gmail.com>
2011-11-04 01:59:46 +0100

    updated time table in header

--- header.php
+++ header.php
@@ -1,8 +1,8 @@
         <div id="container">
             <div id="header" class="central">
-                <div id="logo"><img src="images/logo.png" width="310" height="90" alt="DNU"><div id="op_hours"><h4>orario apertura impianto</h4>lun-mer-ven: 10:00 - 13:30 / 15:00 - 22:00<br>
-mar-gio: 14:00 - 22:00<br>
-sab: 10:00 - 13:30</div></div>
+                <div id="logo"><img src="images/logo.png" width="310" height="90" alt="DNU"><div id="op_hours"><h4>orario apertura impianto</h4>lun-mer-ven: 10:30 - 13:00 / 15:00 - 22:00<br>
+mar-gio: 13:00 - 22:00<br>
+sab: 10:00 - 19:30</div></div>
             </div>
             <div id="menu" class="central">
                         <img src="images/papera.png" width="48" height="30" alt="" id="duck">
