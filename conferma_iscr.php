ded0dc4c008841a63e84638ea89c1cfb276aed74
Stéphane Bisinger <stephane.bisinger@gmail.com>
2011-11-14 15:59:12 +0100

    Created subscription page

---
+++ conferma_iscr.php
@@ -0,0 +1,24 @@
+<?php
+require_once "menu.php";
+require_once "settings.php";
+?>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN"
+"http://www.w3.org/TR/html4/strict.dtd">
+<html>
+    <head>
+        <title>Iscrizione inviata - Ducato Nuoto Urbino</title>
+        <meta http-equiv="Content-type" content="text/html;charset=UTF-8">
+
+<?php include "styles.php"; ?>
+<?php include "js.php"; ?>
+    </head>
+    <body>
+<?php include "header.php"; ?>
+        <div id="body" class="central">
+            <h1>Iscrizione inviata</h1>
+            La domanda di iscrizione è stata inoltrata alla nostra segreteria. Per completare l'iscrizione è necessario presentarsi in segreteria (lun.-ven. dalle 16 alle 20) per firmare la domanda stampata e pagare la quota di iscrizione.<br>
+            <strong>Benvenuto nella Ducato Nuoto Urbino!</strong>
+        </div>
+<?php include "footer.php"; ?>
+    </body>
+</html>
