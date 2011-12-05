2d1bc4db38b07db3f49a75d0ae18b80160e7127d
Stéphane Bisinger <stephane.bisinger@gmail.com>
2011-12-05 22:55:14 +0100

    Imported Symfony base

--- footer.php
+++
@@ -1,5 +0,0 @@
-            <div id="footer" class="central">
-            <hr>
-            ASD Ducato Nuoto Urbino, Via L. Battiferri, 15 - 61029 Urbino (PU). Sede operativa Piazzale F.lli Cervi, 1 - 61029 Urbino (PU) tel./fax 0722 320 834 cell. 328 748 96 95 e-mail: <a href="mailto:ducatonuoto@gmail.com">ducatonuoto@gmail.com</a> - Pagina Facebook: <a href="http://www.facebook.com/pages/Ducato-Nuoto-Urbino/146388187392"><img src="images/facebook.png" width="20" height="20" alt="Ducato Nuoto Urbino" title="Ducato Nuoto Urbino"></a>
-            </div>
-        </div>
