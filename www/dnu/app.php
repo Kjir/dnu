31f23c6079b1e676a863239a6c0d9afd4264e222
Stéphane Bisinger <stephane.bisinger@gmail.com>
2011-12-06 14:34:37 +0100

    Changed structure to ease deployment on sbisinger.net

--- www/dnu/app.php
+++ www/dnu/app.php
@@ -1,8 +1,8 @@
 <?php
 
-require_once __DIR__.'/../app/bootstrap.php.cache';
-require_once __DIR__.'/../app/AppKernel.php';
-//require_once __DIR__.'/../app/AppCache.php';
+require_once __DIR__.'/../../sf2/app/bootstrap.php.cache';
+require_once __DIR__.'/../../sf2/app/AppKernel.php';
+//require_once __DIR__.'/../../sf2/app/AppCache.php';
 
 use Symfony\Component\HttpFoundation\Request;
 
